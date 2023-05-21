f5740fa5254c19d9a99d77de22ee2010a8d93a58
Ayesha <ayesha.glnt012@gmail.com>
2023-05-22 00:00:24 +0200

    one decimal place

--- script/index.js
+++ script/index.js
@@ -7,23 +7,25 @@ function calculate() {
 
     // display results to DOM
     let result = (weight / (height * height) * 10000)
-         document.getElementById('onscreen').innerHTML= result; 
+         document.getElementById('onscreen').innerHTML= result.toFixed(1);
 
-        // if (result < 18.4 || result >= (18.5, 24.9)) {
-        //     document.getElementById("onscreen").innerHTML = 'Your BMI is '+result+'. Unfortunately you are underweight :(';
+        if (result.toFixed(1) < 18.4 || result.toFixed(1) >= (18.5, 24.9)) {
+            document.getElementById("onscreen").innerHTML = 'Your BMI is '+result.toFixed(1)+'. Unfortunately you are underweight :(';
             
-        // } 
-        // else {
-        //     document.getElementById("onscreen").innerHTML = "Good job! Your BMI is "+result+". Your body weight is normal.";
-        // }
-
-        if (result <= (25, 29.9) || result >= 30) {
-            document.getElementById("onscreen").innerHTML = 'Your BMI is '+result+'. Unfortunately you are overweight. :(';
         } 
-
         else {
-            document.getElementById("onscreen").innerHTML = 'Your BMI is '+result+'. You are obese!';
+            document.getElementById("onscreen").innerHTML = "Good job! Your BMI is "+result.toFixed(1)+". Your body weight is normal.";
         }
+
+        // (result >= (25, 29.9) || result >= 30) ? ". Unfortunately you are overweight. :(":". You are obese!"
+        // document.getElementById("onscreen").innerHTML = "Your BMI is "+result;
+        // if (result >= (25, 29.9) || result >= 30) {
+        //     document.getElementById("onscreen").innerHTML = 'Your BMI is '+result+'. Unfortunately you are overweight. :(';
+        // } 
+
+        // else {
+        //     document.getElementById("onscreen").innerHTML = 'Your BMI is '+result+'. You are obese!';
+        // }
 }
 
 // function to clear everything
